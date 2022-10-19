cf741c17166438882e08a8f6fb0d70eb01a81e19
Tasha L <natasha.lewis.lalor@gmail.com>
2022-10-19 18:58:31 -0500

    Finished Studio on 10/12, forgot to commit and push

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        string error = "It's broken";
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            //ViewBag.Error = "Passwords must match.";
+            return View();
+        }
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (newUser.Password == verify)
+            {
+                //ViewBag.verify = verify;
+                ViewBag.ThisUser = newUser;
+
+                return View("Index");
+            } else
+            {
+                ViewBag.Error = error;
+                ViewBag.username = newUser.Username;
+                ViewBag.email = newUser.Email;
+                return View("Add");
+            }
+        }
+
+    }
+}
